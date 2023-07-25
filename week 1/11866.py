0c18c4daf46c18972e88408c3bf4991e04b59fd1
김동현 <silverstuff@gmail.com>
2023-07-25 17:53:20 +0900

    마음이 꺾임

---
+++ week 1/11866.py
@@ -0,0 +1,20 @@
+#리스트를 만들어서 중복값을 반복해서 빼려했으나 연산시간 초과될게 뻔하여 폐기
+
+# a, b = map(int, input().split()) 
+
+# my_list = list(range(1,a+1))
+# rpt_list = my_list*a
+
+# for i in range(a):
+#     while True:
+#         rpt_list.remove(int(rpt_list[(b*(i+1))-1]))
+#         if (rpt_list[(b*(i+1))-1]) == '':
+#             continue
+# print(rpt_list)
+
+
+# ---------------------------------------------------------------
+
+# 2차 시도
+
+
